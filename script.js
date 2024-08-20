d55cc8b035d887a22f80a741d9d272fbb0c0b31c
sapta <saptamabar@gmail.com>
2024-08-20 16:02:12 +0700

    semoga bener

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.getElementById('submitComment').addEventListener('click', function() {
 });
 
 function sendCommentToApi(commentData) {
-    fetch('https://script.googleusercontent.com/macros/echo?user_content_key=V9JeYDk9vJ2ItOnFQM-F-p-ptO6AF4JPmMyfrSIIVMNT3tqNnaAgesJpPDIfnZdbYvG6eJqIbWGdvsr-31KBkJ5ORqqYZtbbm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFvRNOdiby_HrUyPlD2D3sHQt93idbqJTPDy-RGqyDonqeZt4UdJKWg-W_bkwty_vsbbOyyUvNFqMLbLGNSJV8ptb4_rtiTZgQ&lib=M3BV_JfkCOKUIJEGFZkePBuzzWc9fmsux', {
+    fetch('https://script.googleusercontent.com/macros/echo?user_content_key=JVOMzD2Ys5Nfl_gbrrz3TmbbtXVC8mNaPfFph_oi0zgzmvrUdx04Z9fiHATyHWepkO1Moabwe60OnOylFn_lty9On256Z2hYm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFvRNOdiby_HrUyPlD2D3sHQt93idbqJTPDy-RGqyDonqeZt4UdJKWg-W_bkwty_vsbbOyyUvNFqMLbLGNSJV8ptb4_rtiTZgQ&lib=M3BV_JfkCOKUIJEGFZkePBuzzWc9fmsux', {  // Ganti dengan URL Web App yang baru
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -27,11 +27,12 @@ function sendCommentToApi(commentData) {
     .then(response => response.json())
     .then(data => {
         if (data.success) {
-            loadComments();
+            loadComments(); // Fungsi ini untuk memuat ulang komentar setelah berhasil
         }
     })
     .catch(error => console.error('Error:', error));
 }
+
 function loadComments() {
     fetch('https://script.googleusercontent.com/macros/echo?user_content_key=JVOMzD2Ys5Nfl_gbrrz3TmbbtXVC8mNaPfFph_oi0zgzmvrUdx04Z9fiHATyHWepkO1Moabwe60OnOylFn_lty9On256Z2hYm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFvRNOdiby_HrUyPlD2D3sHQt93idbqJTPDy-RGqyDonqeZt4UdJKWg-W_bkwty_vsbbOyyUvNFqMLbLGNSJV8ptb4_rtiTZgQ&lib=M3BV_JfkCOKUIJEGFZkePBuzzWc9fmsux')
         .then(response => response.json())
